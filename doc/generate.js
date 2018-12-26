562b67a430ac167765a1b6a91469afb14cb230e6
Linus Lee <linus@thesephist.com>
2018-12-26 01:41:58 -0500

    Add better styles to autogenerated docs

--- doc/generate.js
+++ doc/generate.js
@@ -6,6 +6,7 @@ const marked = require('marked');
 const mkdirp = require('mkdirp');
 
 const index = fs.readFileSync('./doc/index.html', 'utf8');
+const css = fs.readFileSync('./doc/main.css', 'utf8');
 const template = fs.readFileSync('./doc/template.html', 'utf8');
 const torusSource = fs.readFileSync('./src/torus.js', 'utf8');
 const jdomSource = fs.readFileSync('./src/jdom.js', 'utf8');
@@ -68,6 +69,9 @@ mkdirp.sync('./docs/');
 
 fs.writeFile('./docs/index.html', index, 'utf8', (err) => {
     if (err) console.error('Error writing index page', err);
+});
+fs.writeFile('./docs/main.css', css, 'utf8', (err) => {
+    if (err) console.error('Error writing main.css', err);
 });
 fs.writeFile('./docs/torus.html', buildAnnotatedPage('torus.js', linesToRows(torusSource)), 'utf8', (err) => {
     if (err) console.error('Error writing documentation page', err);

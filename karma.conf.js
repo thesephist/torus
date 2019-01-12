9d0525b9adf1a0c45f376858427ac6f9c3cef1fb
Linus Lee <linus@thesephist.com>
2019-01-12 08:53:12 -0500

    test: move to using one Karma config file for testing, with environment variables

---
+++ karma.conf.js
@@ -0,0 +1,59 @@
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+let karmaFiles;
+if (IS_PROD) {
+    karmaFiles = [
+        'dist/index.min.js',
+        'test/*.js',
+    ];
+} else {
+    karmaFiles = [
+        'dist/torus.no-debug.js',
+        'dist/jdom.dev.js',
+        'test/*.js',
+    ];
+}
+
+let coverageOptions = IS_PROD ? {} : {
+    preprocessors: {
+        'dist/**/*.js': ['coverage'],
+    },
+    coverageReporter: {
+        type: 'html',
+        dir: 'coverage/',
+    },
+};
+
+module.exports = function (config) {
+    config.set({
+
+        frameworks: ['mocha', 'chai'],
+
+        files: karmaFiles,
+
+        exclude: [
+            '**/*.swp'
+        ],
+
+        reporters: IS_PROD ? [
+            'mocha',
+        ] : [
+            'mocha',
+            'coverage',
+        ],
+
+        ...coverageOptions,
+
+        browsers: [
+            'ChromeHeadless',
+        ],
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+    });
+}
